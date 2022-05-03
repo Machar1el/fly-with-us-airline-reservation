9b197d7a8d5cd2c4c458fb517863f7dd818fbba1
Maxime Rochard <maxime.rochard@scalian.com>
2022-05-04 00:41:59 +0200

    [AR-0](fix) Test configuration
    Stopped Spring Application from firing-up for test-running purposes

--- src/test/java/com/flywithus/airlinereservations/AirlineReservationsApplicationTests.java
+++ src/test/java/com/flywithus/airlinereservations/AirlineReservationsApplicationTests.java
@@ -1,25 +1,19 @@
 package com.flywithus.airlinereservations;
 
-import com.flywithus.airlinereservations.controller.UserController;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 class AirlineReservationsApplicationTests {
 
-	@Autowired
-	UserController userController;
-
 	@Test
-	@DisplayName("Context loads and UserController exists")
-	void contextLoads() {
-		Assertions.assertThat(userController).isNotNull();
+	@DisplayName("Context loads")
+	void contextLoads(ApplicationContext context) {
+		Assertions.assertThat(context).isNotNull();
 	}
 
 }
